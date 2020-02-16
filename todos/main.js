4f9c0e01593a5f2ae0807c6bd6df5cf4cbed7ed8
NikitaStepanenko <stepanenkonikit@gmail.com>
2020-02-17 00:39:04 +0300

    17.02

--- todos/main.js
+++ todos/main.js
@@ -1,20 +1,46 @@
 const searchForm = document.querySelector("#search-form");
 const addForm = document.querySelector("#add-form");
 const todoList = document.querySelector(".list");
+const todoOptions = document.querySelector('.todo-options')
 
 todoList.addEventListener("click", e => {
   event.preventDefault();
   if (e.target.classList.contains("delete")) {
-    // todoList.firstChild.remove();
-    console.log(e.target.parentNode);
+
     e.target.parentNode.parentNode.remove();
   }
-  if (e.target.classList.contains("icon-check")) {
-    // todoList.firstChild.remove();
-    console.log(e.target.parentNode);
-    e.target.parentNode.parentNode.remove();
+  if (e.target.classList.contains("icon-check") && !e.target.parentNode.parentNode.parentNode.classList.contains("cross-out") ) {
+    e.target.parentNode.parentNode.parentNode.classList.add("cross-out");
+  }
+  else if (e.target.parentNode.parentNode.parentNode.classList.contains("cross-out")){
+    e.target.parentNode.parentNode.parentNode.classList.remove("cross-out");
   }
 });
+
+todoOptions.addEventListener("click", e=> {
+  event.preventDefault();
+  const list = Array.from(todoList.children);
+  if (e.target.classList.contains("all-todos")) {
+    list.forEach(todo => todo.classList.remove("hide"))
+  }
+  else if (e.target.classList.contains("complited-todos")) {
+    list.forEach(todo => { if(todo.classList.contains("cross-out")) {
+      todo.classList.remove("hide");
+    }
+    else {
+      todo.classList.add("hide");
+    }
+    })
+  }
+  else if (e.target.classList.contains("active-todos")) {
+    list.forEach(todo => { if(todo.classList.contains("cross-out")) {
+      todo.classList.add("hide");
+    }
+    else {
+      todo.classList.remove("hide");
+    }
+  })
+}})
 
 searchForm.addEventListener("keyup", () => {
   event.preventDefault();
@@ -30,34 +56,18 @@ addForm.addEventListener("submit", () => {
 });
 
 function addGoal(value) {
-  let characters = /[A-Za-z0-9]/;
+  let characters = /[A-Za-z0-9А-Яа-я]/;
   if (characters.test(value)) {
     const pattern = ` 
    <li class="goal-from-list">
   <div class="stroke">
     <div class="goal">
-      <span class="icon-check" 
-        ><svg
-          aria-hidden="true"
-          focusable="false"
-          data-prefix="fas"
-          data-icon="check-circle"
-          class="svg-inline--fa fa-check-circle fa-w-16"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 512 512"
-        >
-          <path
-            fill="currentColor"
-            d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
-          ></path></svg
-      ></span>
+      <div class="icon-check"></div>
       <p class="todo-content">
       ${value}
       </p>
     </div>  
     <div class=" delete icon-trash">
-    
     </div>
   </div>
 </li>`;
@@ -66,18 +76,6 @@ function addGoal(value) {
 }
 
 function searchGoal(word) {
-  // if (word === "" || word === null) {
-  //   filteredList.forEach(todo => todo.classList.remove("hide"));
-  // } else {
-  //   console.log(word);
-  //   const list = Array.from(todoList.children);
-  //   const filteredList = list.filter(
-  //     todo => !todo.querySelector(".todo-content").innerText.includes(word)
-  //   );
-
-  //   filteredList.forEach(todo => todo.setAttribute("class", "hide"));
-
-  // showNewList(filt);
   const list = Array.from(todoList.children);
   list.forEach(todo => {
     const content = todo.querySelector(".todo-content").innerText.toLowerCase();
