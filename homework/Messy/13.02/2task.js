0b96e3962238b262531f00ceb480fc092878fa29
NikitaStepanenko <stepanenkonikit@gmail.com>
2020-02-17 17:57:17 +0300

    17.02

--- homework/Messy/13.02/2task.js
+++ homework/Messy/13.02/2task.js
@@ -1,20 +1,29 @@
 // Select all the list items on the page and convert to array
 
+const list = Array.from(document.querySelectorAll("li"));
+
 // Filter for only the elements that contain the word 'Flexbox'
+
+const filt = list.filter(x => x.textContent.includes("Flexbox"));
 
 // map down to a list of time strings
 
-// map to an array of seconds
+const map = filt
+  .map(x => x.dataset.time)
+
+  // map to an array of seconds
+
+  .map(x => {
+    time = x.split(":");
+    return parseInt(time[0] * 60) + parseInt(time[1]);
+  });
+
+console.log(map);
 
 // reduce to get total
 
+answer = map.reduce((total, val) => total + val);
+
 // 🔥 This can also be done in a single .reduce(), but we're practicing arrow functions here, so chain them!
 
-const list = Array.from(document.querySelectorAll("li"));
-// const arr = [...list]
-
-const filt = list.filter(x => x.textContent.includes('Flexbox').map(x => x.dataset.time);
-
-for (x in map) {
-  console.log(map[x]);
-}
+console.log(answer);
--- todos/main.js
+++ todos/main.js
@@ -1,46 +1,48 @@
 const searchForm = document.querySelector("#search-form");
 const addForm = document.querySelector("#add-form");
 const todoList = document.querySelector(".list");
-const todoOptions = document.querySelector('.todo-options')
+const todoOptions = document.querySelector(".todo-options");
 
 todoList.addEventListener("click", e => {
   event.preventDefault();
   if (e.target.classList.contains("delete")) {
-
     e.target.parentNode.parentNode.remove();
   }
-  if (e.target.classList.contains("icon-check") && !e.target.parentNode.parentNode.parentNode.classList.contains("cross-out") ) {
+  if (
+    e.target.classList.contains("icon-check") &&
+    !e.target.parentNode.parentNode.parentNode.classList.contains("cross-out")
+  ) {
     e.target.parentNode.parentNode.parentNode.classList.add("cross-out");
-  }
-  else if (e.target.parentNode.parentNode.parentNode.classList.contains("cross-out")){
+  } else if (
+    e.target.parentNode.parentNode.parentNode.classList.contains("cross-out")
+  ) {
     e.target.parentNode.parentNode.parentNode.classList.remove("cross-out");
   }
 });
 
-todoOptions.addEventListener("click", e=> {
+todoOptions.addEventListener("click", e => {
   event.preventDefault();
   const list = Array.from(todoList.children);
   if (e.target.classList.contains("all-todos")) {
-    list.forEach(todo => todo.classList.remove("hide"))
+    list.forEach(todo => todo.classList.remove("hide"));
+  } else if (e.target.classList.contains("complited-todos")) {
+    list.forEach(todo => {
+      if (todo.classList.contains("cross-out")) {
+        todo.classList.remove("hide");
+      } else {
+        todo.classList.add("hide");
+      }
+    });
+  } else if (e.target.classList.contains("active-todos")) {
+    list.forEach(todo => {
+      if (todo.classList.contains("cross-out")) {
+        todo.classList.add("hide");
+      } else {
+        todo.classList.remove("hide");
+      }
+    });
   }
-  else if (e.target.classList.contains("complited-todos")) {
-    list.forEach(todo => { if(todo.classList.contains("cross-out")) {
-      todo.classList.remove("hide");
-    }
-    else {
-      todo.classList.add("hide");
-    }
-    })
-  }
-  else if (e.target.classList.contains("active-todos")) {
-    list.forEach(todo => { if(todo.classList.contains("cross-out")) {
-      todo.classList.add("hide");
-    }
-    else {
-      todo.classList.remove("hide");
-    }
-  })
-}})
+});
 
 searchForm.addEventListener("keyup", () => {
   event.preventDefault();
@@ -59,18 +61,18 @@ function addGoal(value) {
   let characters = /[A-Za-z0-9А-Яа-я]/;
   if (characters.test(value)) {
     const pattern = ` 
-   <li class="goal-from-list">
-  <div class="stroke">
-    <div class="goal">
-      <div class="icon-check"></div>
-      <p class="todo-content">
-      ${value}
-      </p>
-    </div>  
-    <div class=" delete icon-trash">
+    <li class="goal-from-list">
+    <div class="stroke">
+      <div class="goal">
+        <div class="icon-check"></div>
+        <p class="todo-content">
+        ${value}
+        </p>
+      </div>  
+      <div class=" delete icon-trash">
+      </div>
     </div>
-  </div>
-</li>`;
+  </li>`;
     todoList.innerHTML += pattern;
   }
 }
@@ -84,7 +86,5 @@ function searchGoal(word) {
     } else {
       todo.classList.remove("hide");
     }
-    console.log("todo", todo.querySelector(".todo-content").innerText);
   });
-  console.log("filteredTodoList", list);
 }
